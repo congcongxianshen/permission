e778ede6901c0e6d61b8a517e7546582ad3ef3bc
fanyalong <fanyalong@wondertek.com.cn>
2018-11-12 17:41:59 +0800

    ssm测试小栗子

---
+++ src/test/java/comkj/permission/controller/UserControllerTest.java
@@ -0,0 +1,15 @@
+package comkj.permission.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class UserControllerTest {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
